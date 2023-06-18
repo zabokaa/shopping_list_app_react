d9dbea08ab2635eaabd9b788852792b1ae3a298d
zabokaa <>
2023-06-18 21:07:20 +0200

    Created list with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import "./styles.css";
 
 export default function App() {
-  return <form className="new-item-form"> 
-    <div className="form-row">
-      <label htmlFor="item">new item</label>
-      <input type="text" id="item" />
-    </div>
-    <button className="btn">add to list</button>
-  </form>
+  return (
+    // IN JSX use fragment, it is an empty tag
+    // className
+  <><form className="new-item-form">   
+      <div className="form-row">
+        <label htmlFor="item">new item</label>
+        <input type="text" id="item" />
+      </div>
+      <button className="btn">add to list</button>
+    </form>
+     <h1 className="header">shopping list</h1>
+     <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 1
+          </label>
+          <button className="btn btn-danger">already bought</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 2
+          </label>
+          <button className="btn btn-danger">already bought</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 3
+          </label>
+          <button className="btn btn-danger">already bought</button>
+        </li>
+     </ul>
+  </>
+  )
 }
